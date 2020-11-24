e46a3a76c4a6ed5af10bc243b8fa1d2771f7d3c0
IvanAKuzhelelev <kuzhelevdesign@gmail.com>
2020-11-24 23:09:01 +0300

    toggle principle

--- src/components/BasicToggle.jsx
+++ src/components/BasicToggle.jsx
@@ -1,14 +1,37 @@
-import React from "react"
-import styles from "./BasicToggle.module.css"
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { css } from "@emotion/react";
 
 const BasicToggle = () => {
-    const Colors = ["blue", "pink"];
-    return (
-        <React.Fragment>
-
-        <input type="checkbox" id="toggle" />
-        <label for="toggle"/>
-        </React.Fragment>
-    )
-}
+  const Colors = ["blue", "pink"];
+  return (
+    <React.Fragment>
+      <div
+        css={css`
+          display: inline-block;
+        `}
+      >
+        <input
+          type="checkbox"
+          id="toggle"
+          css={css`
+            :checked + label {
+              background-color: #2e2635;
+            }
+          `}
+        />
+        <label
+          htmlFor="toggle"
+          css={css`
+            width: 6rem;
+            height: 2rem;
+            display: block;
+            background-color: violet;
+            border-radius: 50px;
+          `}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 export default BasicToggle;
